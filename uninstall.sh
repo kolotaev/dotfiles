3d12e16fe0d30c56a8d4351d50f1b104dbf9f3d7
kolotaev <kolotaev@tut.by>
2016-11-01 22:19:25 +0300

    uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -2,3 +2,45 @@
 
 BAK_DIR="$HOME/.original_dotfiles"
 
+# delete symlinks
+function delete_symlinks() {
+  ignoredfiles=(README.md install.sh, uninstall.sh)
+
+  for f in $(ls -d *); do
+    if [[ ${ignoredfiles[@]} =~ $f ]]; then
+      echo "Deleting file $f from ~"
+      rm "$HOME/.$f"
+    fi
+  done
+
+  for f in $(ls -A $BAK_DIR); do
+    echo "Putting backed-up file $f from back to ~"
+    mv $f "$HOME/$f"
+  done
+}
+
+# restore backed-up files
+function restore_from_backup() {
+  for f in $(ls -A $BAK_DIR); do
+    echo "Putting backed-up file $f from back to ~"
+    mv $f "$HOME/$f"
+  done
+}
+
+echo "Setting up Operating System..."
+
+set -e
+(
+  echo "Uninstalling dotfiles..."
+  delete_symlinks
+
+  echo "Restoring from backup"
+  restore_from_backup
+
+  echo "Removing $BAK_DIR - it's not longer needed"
+  rm $BAK_DIR
+
+  echo "Operating System rollback complete."
+  echo "Reloading session..."
+  exec $SHELL -l
+)
