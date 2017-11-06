dacd631e11a525b27d1f4447234c2d822da6631a
kolotaev <kolotaev@tut.by>
2017-11-06 18:25:47 +0300

    Add leiningen default profile with lint plugins

---
+++ lein/profiles.clj
@@ -0,0 +1,4 @@
+; This is a global leiningen profile that is applied to all projects.
+{:user {:plugins [[jonase/eastwood "0.2.5"]
+                             [lein-kibit "0.1.5"]
+                             [lein-cljfmt "0.5.7"]]}}
