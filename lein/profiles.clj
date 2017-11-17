69c8adb5d04cefdcc1dcdc56d42d7abeb32577ec
kolotaev <kolotaev@tut.by>
2017-11-17 21:45:38 +0300

    Use clojure pretty stacktrace

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,7 @@
-; This is a global leiningen profile that is applied to all projects.
+; This is global leiningen profile that is applied to all projects.
 {:user {:plugins [[jonase/eastwood "0.2.5"]
-                             [lein-kibit "0.1.5"]
-                             [lein-cljfmt "0.5.7"]]}}
+                  [lein-kibit "0.1.5"]
+                  [lein-cljfmt "0.5.7"]
+                  ; [venantius/ultra "0.5.2"]
+                  [io.aviso/pretty "0.1.34"]]
+        :dependencies [[io.aviso/pretty "0.1.34"]]}}
