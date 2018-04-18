4bd43fcfcb15348dbc1d47939369d208d7988151
kolotaev <kolotaev@tut.by>
2018-04-18 17:43:51 +0300

    Delete vendor dir from gitignore

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,7 +3,8 @@
                   [lein-kibit "0.1.5"]
                   [lein-cljfmt "0.5.7"]
                   ; [venantius/ultra "0.5.2"]
-                  [io.aviso/pretty "0.1.34"]]
+                  [io.aviso/pretty "0.1.34"]
+                  [lein-cloverage "1.0.10"]]
         :dependencies [[io.aviso/pretty "0.1.34"]]
 
         :cljfmt {:remove-consecutive-blank-lines? false}}}
