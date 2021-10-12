bf89e3ac4f6c6e98f04d096fdd40e0a3af28ee40
kolotaev <kolotaev@tut.by>
2021-10-12 12:06:37 +0300

    Update cljfmt

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
 ; This is global leiningen profile that is applied to all projects.
 {:user {:plugins [[jonase/eastwood "0.2.5"]
                   [lein-kibit "0.1.5"]
-                  [lein-cljfmt "0.5.7"]
+                ;;   [lein-cljfmt "0.8.0"]
                   ; [venantius/ultra "0.5.2"]
                   [nightlight/lein-nightlight "1.9.3"]
                   [io.aviso/pretty "0.1.37"]
